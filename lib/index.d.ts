10885650de0c305f29a3593221551934f26d5541
binhkhuu <binhkhuu90@gmail.com>
2021-06-04 22:02:15 +0800

    testing

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,2 @@
 export declare const Greeter: (name: string) => string;
+export declare function findEmojis(str: string): string[];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+
+import { toShort } from 'emojione';
+const EMOJI_SHORTCODES = /:[a-zA-Z1-9_]+:/g
+
+
 export const Greeter = (name: string) => `Hello ${name}`;
+export function findEmojis(str: string): string[] {
+  // add runtime check for use in JavaScript
+  if (typeof str !== 'string') {
+    return [];
+  }
+
+  return toShort(str).match(EMOJI_SHORTCODES) || [];
+}
